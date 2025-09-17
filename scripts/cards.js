e8e3664434eb9b398fe4ca32c125bac33edd1116
Airam21170247 <olaairam@gmail.com>
2025-09-17 11:01:28 -0700

    Empty Commit

--- scripts/cards.js
+++ scripts/cards.js
@@ -14,7 +14,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-    const urlParams = new URLSearchParams(window.location.search);
+const urlParams = new URLSearchParams(window.location.search);
     const setId = urlParams.get("setId");
 
     let cards = [];
